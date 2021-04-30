996c6391b73339557c8c68c8dbef90a826ef2a93
HarryPotter <llofmejb@gmail.com>
2021-04-30 19:32:04 +0300

    class practice

---
+++ pratik3.py
@@ -0,0 +1,2 @@
+liste = [3, 6, 9, 12, 15, 18]
+print([i for i in liste if i%2])
---
+++ pratik4.py
@@ -0,0 +1,10 @@
+def calculate(a, b, c):
+    if c == "+":
+        print(a+b)
+    elif c == "-":
+        print(a-b)
+    elif c == "/":
+        print(a/b)
+    elif c == "*":
+        print(a*b)
+calculate(21,3,"/")
