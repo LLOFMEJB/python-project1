b0b240484c94893d210d98e9e9b5f3dfc8cebd56
HarryPotter <llofmejb@gmail.com>
2021-05-01 22:19:05 +0300

    teamwork

--- pratik4.py
+++ pratik4.py
@@ -7,4 +7,10 @@ def calculate(a, b, c):
         print(a/b)
     elif c == "*":
         print(a*b)
+    elif c == "%":
+        print(a%b)
+    elif c == "//":
+        print(a//b)
+    else:
+        print("Enter a valid operator!")
 calculate(21,3,"/")
