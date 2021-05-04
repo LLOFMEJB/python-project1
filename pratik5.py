2761d2b0b3a43ef98f803ace9457dc4d7537a9e8
HarryPotter <llofmejb@gmail.com>
2021-05-04 13:25:43 +0300

    __doc__

--- pratik5.py
+++ pratik5.py
@@ -1,7 +1,11 @@
 def absolute_value(value):
+    """
+    no negativity please
+    """
     if value < 0:
         return -value
     else:
         return value
 value = int(input("write the value: "))
-print(absolute_value(value))    
+print(absolute_value(value))
+print(absolute_value.__doc__)
